83431eea33011da4c22839c1615be59d06c41c5d
Th3PaSt3qu3 <65755297+Th3PaSt3qu3@users.noreply.github.com>
2020-12-21 13:42:28 +0100

    On avance, qques trucs à vérif

--- main/knowledge_basis.h
+++ main/knowledge_basis.h
@@ -7,3 +7,30 @@ typedef struct BC{
 
 typedef ElemBC* BC;
 
+/*
+ * Prototypes : 
+ */
+
+/********************************
+ * @brief fonction de création d'une base de connaissance
+ * 
+ * @return base de connaissance nouvellement créée
+ */
+BC createBasis();
+
+/********************************
+ * @brief fonction d'ajout de règle à une base
+ * 
+ * @param knowledge_basis variable de type BC acceuillant la base de connaissance
+ * @param r variable de type règle à intégrer à BC
+ * @return pas de return
+ */
+void addRuleBasis(BC knowledge_basis, Regle r);
+
+/********************************
+ * @brief fonction d'accès à la valeur de tète de la base de connaissance
+ * 
+ * @param knoledge_basis variable de type BC acceuillant la base de connaissance
+ * @return la regle qui est valeur de tête
+ */
+Regle headValueBasis(BC knowledge_basis);
--- main/konwledge_basis.c
+++ main/konwledge_basis.c
@@ -1,3 +1,32 @@
+#include "knowledge_basis.h"
 
 /*------------------------------------*/
 
+BC createBasis(){
+    BC new = (BC)malloc(sizeof(ElemBC));
+    return new;
+}
+
+/*------------------------------------*/
+
+ void addRuleBasis(BC knowledge_basis, Regle r){
+     if (knowledge_basis == NULL){
+         knowledge_basis->head = r;
+         knowledge_basis->next = NULL;
+     }
+     else {
+         while(knowledge_basis->next != NULL){
+             knowledge_basis = knowledge_basis->next;
+         }
+         BC new = (BC)malloc(sizeof(ElemBC));
+         new->head = r;
+         knowledge_basis->next = new;
+     }
+ }
+
+ /*------------------------------------*/
+
+ Regle headValueBasis(BC knowledge_basis){
+     return knowledge_basis->head;
+ }
+ 
