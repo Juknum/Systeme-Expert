4a405691710961eaea556227f88f27683c8c39c6
Julien <Juknum@users.noreply.github.com>
2020-12-28 02:25:05 +0100

    Spaces fix

--- main/fact_basis.c
+++ main/fact_basis.c
@@ -1,11 +1,10 @@
 #include "fact_basis.h"
 
 BF createFactBasis(){
-    BF fact_basis = NULL;
-    return fact_basis;
+	BF fact_basis = NULL;
+	return fact_basis;
 }
 
 Bool isEmptyFactBasis(BF fact_basis){
-    return fact_basis == NULL;
+	return fact_basis == NULL;
 }
-
--- main/konwledge_basis.c
+++ main/konwledge_basis.c
@@ -3,30 +3,30 @@
 /*------------------------------------*/
 
 BC createBasis(){
-    BC new = (BC)malloc(sizeof(ElemBC));
-    return new;
+	BC new = (BC)malloc(sizeof(ElemBC));
+	return new;
 }
 
 /*------------------------------------*/
 
  BC addRuleBasis(BC knowledge_basis, Regle r){
-     if (knowledge_basis == NULL){
-         knowledge_basis->head = r;
-         knowledge_basis->next = NULL;
-     }
-     else {
-         while(knowledge_basis->next != NULL){
-             knowledge_basis = knowledge_basis->next;
-         }
-         BC new = (BC)malloc(sizeof(ElemBC));
-         new->head = r;
-         knowledge_basis->next = new;
-     }
+	if (knowledge_basis == NULL){
+		knowledge_basis->head = r;
+		knowledge_basis->next = NULL;
+	}
+	else {
+		while(knowledge_basis->next != NULL){
+			knowledge_basis = knowledge_basis->next;
+		}
+		BC new = (BC)malloc(sizeof(ElemBC));
+		new->head = r;
+		knowledge_basis->next = new;
+	}
  }
 
  /*------------------------------------*/
 
  Regle headValueBasis(BC knowledge_basis){
-     return knowledge_basis->head;
+	return knowledge_basis->head;
  }
  
