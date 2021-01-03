a47ef1fe84cbb2b517d7be5e73d01ee596f674f6
Julien <Juknum@users.noreply.github.com>
2021-01-03 01:31:29 +0100

    Update compatibility.h

--- headers/compatibility.h
+++ headers/compatibility.h
@@ -2,10 +2,7 @@
 #ifdef _WIN32
 
 #include <windows.h>
-/*
-#define pause 				Sleep(1000)
-#define pause_load 		Sleep(10)
-*/
+
 #define CLEAR system("cls")
 #define ASCII	system("chcp 65001")
 
@@ -14,11 +11,6 @@
 // LINUX USERS
 #ifdef linux
 
-/*
-#include <unistd.h>
-#define pause 				sleep(1)
-#define pause_load 		sleep(0.1)
-*/
 #include <stdio.h>
 #define CLEAR	printf("\x1b[H\x1b[J")
 #define ASCII	NULL
